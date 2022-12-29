3420e7c5ff56313bdd7cfed5984bb18ce5c88297
lucy2023 <121642901+lucy2023@users.noreply.github.com>
2022-12-29 21:35:15 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+music.playMelody("- - - - - - - - ", 120)
+basic.forever(function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # . # .
+        . # # # .
+        `)
+})
